bcf41c3269c02ab0d4022c95426471d447900e24
Ever Eduardo Chicas Prado <2373708320101@ingenieria.usac.edu.gt>
2021-03-03 19:38:02 -0600

    add script task

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,17 +1,19 @@
 CREATE DATABASE HT2;
 USE HT2;
 
-CREATE TABLE temporal(
+CREATE TABLE Temporal(
 	Carne VARCHAR(200),
 	Nombre  VARCHAR(200),
 	LlevaLab  VARCHAR(200),
 	PosibleNota  VARCHAR(200)
 );
 
-CREATE TABLE alumno(
+CREATE TABLE Alumno(
 	IdAlumno INT IDENTITY(1,1) PRIMARY KEY,
 	Carne INT,
 	Nombre VARCHAR(200),
 	LlevaLab VARCHAR(200),
 	PosibleNota INT
 );
+
+SELECT * FROM Temporal;
